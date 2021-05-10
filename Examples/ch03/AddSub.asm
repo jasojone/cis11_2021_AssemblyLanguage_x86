6c6bb62c7a8b47da1b4ecbbe9f1800e57615901d
Jason Jones <jasojone@gmail.com>
2021-05-09 20:27:22 -0700

    working on extra credit for week 3

--- Examples/ch03/AddSub.asm
+++ Examples/ch03/AddSub.asm
@@ -1,40 +1,175 @@
-
 COMMENT!
 Student:  Jason Jones
 Class : cis11
-hw2b v1, v2
+extra credit2
+
+Create a new programand include the following :
+
+1.   Declare two 4 byte signed integer variables named : num1 and num2
+2.   Prompt the user to enter two numbers.   (See output)
+
+3.   Read and assign the inputs to num1 and num2.
+
+4.   Use the ADD instruction to add num1 and num2.
+
+5.   Display the result.   (See output)
+
+6.   Use the SUB instruction to subtract num1 and num2.
+
+7.   Display the result.   (See output)
+
+8.   Use the IMUL instruction to multiply num1 and num2.
+
+9.   Display the result.   (See output)
+
 !
 
-TITLE Addand Subtract(AddSub.asm)
 
 INCLUDE Irvine32.inc
 
 .data
-mathProb BYTE "-5 + 3 = ", 0
-s1 BYTE 'Say "Goodnight" Gracie'
-s2 BYTE "Say 'Goodnight' Gracie"
 
-
-num1 SDWORD - 5
-num2 SDWORD 3
-num3 SDWORD 0
-
-
+num1 SDWORD ?
+num2 SDWORD ?
+total SDWORD ?
+prompt BYTE "Enter a number ", 0
+showEqual BYTE " = ", 0
+showPlus BYTE "num1 + num2", 0
+showMinus BYTE "num1 - num2", 0
+showMul BYTE "num1 * num2", 0
 .code
-main PROC
 
-mov edx, OFFSET s1
-call WriteString;// displays what is in the edx
-mov edx, OFFSET s2
-call WriteString;// displays what is in the edx
+main PROC
+mov edx, OFFSET prompt
 call WriteString
-call crlf
+call ReadInt
+mov num1, eax
+
+call WriteString
+call ReadInt
+mov num2, eax
+
+add eax, num1
+
+mov total, eax
+
+mov edx, OFFSET showPlus
+call WriteString
+
+mov eax, num2
+
+mov edx, OFFSET showEqual
+call WriteString
+
+mov eax, total
+call WriteInt
+call Crlf
+
+mov eax, num1
+mov eax, num2
+
+sub eax, num2
+
+mov total, eax
+call WriteInt
+
+
+mov edx, OFFSET showMinus
+call WriteString
+
+mov eax, num2
+
+mov edx, OFFSET showEqual
+call WriteString
+
+mov eax, total
+call WriteInt
+call Crlf
+
 
 
 exit
 main ENDP
 END main
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+COMMENT!
+Student:  Jason Jones
+Class : cis11
+extra credit 
+
+Declare a 4 byte signed integer variable named : num1
+
+2.   Initialize num1 with a value of 4.
+
+3.   Display the value in num1.   (See output)
+
+4.   Use the INC instruction to add one to the value in num1.
+
+5.   Display the value in num1 after it has been incremented.   (See output)
+
+6.   Use the DEC instruction to decrement the value in num1.
+
+7.   Display the value in num1 after it has been decremented.   (See output)
+
+8.   Use the NEG instruction to negate the value in num1.
+
+9.   Display the value in num1 after it has been negated.   (See output
+
+
+
+
+TITLE Addand Subtract(AddSub.asm)
+
+; This program uses the inc dec and neg Mnemonic operands
+
+INCLUDE Irvine32.inc
+
+.data
+num1 SBYTE 4
+
+.code
+
+main PROC
+
+movzx	eax, num1
+
+call WriteInt
+call Crlf
+inc eax
+call WriteInt
+call Crlf
+dec eax
+call WriteInt
+call Crlf
+neg eax 
+call WriteInt
+call Crlf
+
+
+exit
+main ENDP
+END main
+!
 
 
 comment!
