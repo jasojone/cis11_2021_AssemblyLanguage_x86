e14a6844c00e457b485b9375f5adc3eabbafd20d
Jason Jones <jasojone@gmail.com>
2021-05-29 15:58:17 -0700

    commit

--- Examples/ch04/Moves.asm
+++ Examples/ch04/Moves.asm
@@ -15,34 +15,33 @@ arrayD DWORD 10000h,20000h
 .code
 main PROC
 
-;  MOVZX
+ MOVZX
 	mov    bx,0A69Bh
 	movzx  eax,bx		; EAX = 0000A69Bh
 	movzx  edx,bl		; EDX = 0000009Bh
 	movzx  cx,bl		; CX  = 009Bh
 
-;  MOVSX
+  MOVSX
 	mov   bx,0A69Bh
 	movsx eax,bx		; EAX = FFFFA69Bh
 	movsx edx,bl		; EDX = FFFFFF9Bh
 	mov	 bl,7Bh
 	movsx cx,bl		; CX  = 007Bh
 
-;  Memory-to-memory exchange:
+  Memory-to-memory exchange:
 	mov  ax,val1		; AX = 1000h
 	xchg ax,val2		; AX = 2000h, val2 = 1000h
 	mov  val1,ax		; val1 = 2000h
 
-;  Direct-Offset Addressing (byte array):
+ Direct-Offset Addressing (byte array):
 	mov al,arrayB		; AL = 10h
 	mov al,[arrayB+1]	; AL = 20h
 	mov al,[arrayB+2]	; AL = 30h
 
-;  Direct-Offset Addressing (word array):
+  Direct-Offset Addressing (word array):
 	mov ax,arrayW		; AX = 100h
 	mov ax,[arrayW+2]	; AX = 200h
-
-;  Direct-Offset Addressing (doubleword array):
+  Direct-Offset Addressing (doubleword array):
 	mov eax,arrayD				; EAX = 10000h
 	mov eax,[arrayD+4]			; EAX = 20000h
 	mov eax,[arrayD+TYPE arrayD]	; EAX = 20000h
